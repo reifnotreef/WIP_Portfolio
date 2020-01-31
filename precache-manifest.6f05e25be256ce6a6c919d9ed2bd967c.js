87f72bacc4836ffeff6618d2d3b9b2eea3e6a56e
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2020-01-31 02:00:30 -0600

    Updates

---
+++ precache-manifest.6f05e25be256ce6a6c919d9ed2bd967c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269132b557babffa0d1c5a504dac1f49",
+    "url": "/wip_portfolio/index.html"
+  },
+  {
+    "revision": "d1dfee1542558b5478b0",
+    "url": "/wip_portfolio/static/css/main.60c23a81.chunk.css"
+  },
+  {
+    "revision": "2b5ef2297154be8f3f4f",
+    "url": "/wip_portfolio/static/js/2.b49aa175.chunk.js"
+  },
+  {
+    "revision": "d1dfee1542558b5478b0",
+    "url": "/wip_portfolio/static/js/main.a80e7f8f.chunk.js"
+  },
+  {
+    "revision": "914a89c3a2ab43492ad1",
+    "url": "/wip_portfolio/static/js/runtime-main.ac13c71c.js"
+  }
+]);
